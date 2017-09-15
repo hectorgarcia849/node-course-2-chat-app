a06d39654cf2c2dfdb3f468d8ade55f10283dfbb
hector <hectorgarcia849@gmail.com>
2017-09-15 16:21:04 -0400

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 var socket = io();
 
+function scrollToBottom() {
+  //selectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child');
+  //heights
+  var clientHeight = messages.prop('clientHeight') ;
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if(clientHeight + scrollTop +newMessageHeight + lastMessageHeight >= scrollHeight){
+      messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on('connect', function() {
     console.log('Connected to server');
 });
@@ -13,7 +29,7 @@ socket.on('newMessage', function(message) {
     var template = jQuery('#message-template').html();
     var html = Mustache.render(template, {text: message.text, from: message.from, createdAt: formattedTime});
     jQuery('#messages').append(html);
-
+    scrollToBottom();
 });
 
 socket.on('newLocationMessage', function(message) {
@@ -21,6 +37,7 @@ socket.on('newLocationMessage', function(message) {
     var template = jQuery('#location-message-template').html();
     var html = Mustache.render(template, {from: message.from, createdAt: formattedTime, url: message.url});
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 var messageTextBox = jQuery("[name=message]");
